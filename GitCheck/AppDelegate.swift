dbff0b288312c19f9cd4844b40881689b50c089b
Manjunath H <manjunath@manjunaths-macbook-pro.local>
2018-09-24 18:12:38 +0530

    new brach

--- GitCheck/AppDelegate.swift
+++ GitCheck/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("check now")
+        print("check now prasu brach")
         return true
     }
 
